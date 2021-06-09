0137d33811121e7f6e32e365d7e8b9ebf101be0b
Mario Gonzalez <mario.gonzalez.a2599@gmail.com>
2021-06-08 20:12:52 -0500

    maybe

--- app.py
+++ app.py
@@ -9,24 +9,23 @@ from bson.objectid import ObjectId
 config = dotenv_values(".env")
 client = MongoClient(config.get("DB_URL"))
 db = client.covidmx
-app = None
 
 def create_app():
-    _app = Flask(__name__, static_url_path='')
+    app = Flask(__name__, static_url_path='')
 
-    _app.config['SECRET_KEY'] = config.get("SECRET_KEY")
+    app.config['SECRET_KEY'] = config.get("SECRET_KEY")
 
     # blueprint for auth routes in our app
     from auth import auth as auth_blueprint
-    _app.register_blueprint(auth_blueprint)
+    app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
     from main import main as main_blueprint
-    _app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_blueprint)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
-    login_manager.init_app(_app)
+    login_manager.init_app(app)
 
     from models import User
 
@@ -35,10 +34,8 @@ def create_app():
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User(db.userAuth.find_one({'_id': ObjectId(user_id)}))
 
-    return _app
+    return app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+app = create_app()
 
 
