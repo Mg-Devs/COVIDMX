4275539794524891b0b2e7c2ea5142c8fc06edfd
Mario Gonzalez <mario.gonzalez.a2599@gmail.com>
2021-06-08 19:59:15 -0500

    ahorasi

--- app.py
+++ app.py
@@ -12,21 +12,21 @@ db = client.covidmx
 app = None
 
 def create_app():
-    app = Flask(__name__, static_url_path='')
+    _app = Flask(__name__, static_url_path='')
 
-    app.config['SECRET_KEY'] = config.get("SECRET_KEY")
+    _app.config['SECRET_KEY'] = config.get("SECRET_KEY")
 
     # blueprint for auth routes in our app
     from auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    _app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
     from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    _app.register_blueprint(main_blueprint)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+    login_manager.init_app(_app)
 
     from models import User
 
@@ -35,7 +35,7 @@ def create_app():
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User(db.userAuth.find_one({'_id': ObjectId(user_id)}))
 
-    return app
+    return _app
 
 if __name__ == '__main__':
     app = create_app()
